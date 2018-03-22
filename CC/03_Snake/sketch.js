a0e2a03b809b36aa2d69beecf107589c30f8a79a
MAshraf <ee.mashraf@gmail.com>
2018-03-22 11:06:05 +0200

    fix init

--- CC/03_Snake/sketch.js
+++ CC/03_Snake/sketch.js
@@ -1,12 +1,13 @@
 // globals
+var snake;
 
 // Constants
 const SCREEN_SIZE = 600;
-
+const SNAKE_SIZE = 10;
 // Setup 
 function setup() {
-  createCanvas(SCREEN_SIZE, SCREEN_SIZE, WEBGL);
-
+  createCanvas(SCREEN_SIZE, SCREEN_SIZE);
+  snake = new Snake();
 }
 
 // Control
@@ -14,5 +15,25 @@ function setup() {
 // Draw
 function draw() {
   background(51); // background color
+  snake.update();
+  snake.show();
+}
+
+// Snake Object
+function Snake() {
+  this.x = 0;
+  this.y = 0;
+  this.xSpeed = 1;
+  this.ySpeed = 0;
+  
+  this.update= function() {
+    this.x = this.x + this.xSpeed;
+    this.y = this.y + this.ySpeed;
+  }
+  
+  this.show = function(){
+    fill(255);
+    rect(this.x, this.y, SNAKE_SIZE, SNAKE_SIZE);
+  }
 }
 

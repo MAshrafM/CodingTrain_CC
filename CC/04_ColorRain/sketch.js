9f0da3bc565ace8e39692a90734e9786b0f4b611
MAshraf <ee.mashraf@gmail.com>
2018-03-26 17:30:28 +0200

    cc04 init

---
+++ CC/04_ColorRain/sketch.js
@@ -0,0 +1,21 @@
+// globals
+
+
+// Constants
+const SCREEN_SIZE = 400;
+
+
+// Setup 
+function setup() {
+  createCanvas(SCREEN_SIZE, SCREEN_SIZE);
+
+}
+
+// Control
+
+
+
+// Draw
+function draw() {
+
+}

c929bbfa0a1453b1a71e4bd4f4f39f9c22106ccf
MAshraf <ee.mashraf@gmail.com>
2018-06-24 17:49:08 +0300

    cc05 init

---
+++ CC/05_SpaceInvaders/sketch.js
@@ -0,0 +1,20 @@
+// globals
+
+// Constants
+const SCREEN_SIZE = 600;
+
+
+// Setup 
+function setup() {
+  createCanvas(SCREEN_SIZE, SCREEN_SIZE);
+
+}
+
+// Control
+
+
+
+// Draw
+function draw() {
+
+}

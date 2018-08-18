e200ecba6c366f787e56a50d4cfd9a8e4fecee3a
MAshraf <ee.mashraf@gmail.com>
2018-08-18 23:36:36 +0300

    c12 init

---
+++ CC/12_LorenzAttractor/sketch.js
@@ -0,0 +1,19 @@
+// globals
+
+
+// Constants
+const SCREEN_SIZE = 600;
+
+// Setup 
+function setup() {
+  createCanvas(SCREEN_SIZE, SCREEN_SIZE);
+
+}
+
+// Controls
+
+
+// Draw
+function draw() {
+
+}

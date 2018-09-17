83cdf43452c1cbc0cf18c0f3f446f97ad8e87d30
MAshraf <ee.mashraf@gmail.com>
2018-09-17 12:25:51 +0300

    cc14

---
+++ public/CC/00_Basic/sketch.js
@@ -0,0 +1,17 @@
+// globals
+
+
+// constant
+const SCREEN_SIZE = 400;
+
+// Setup
+function setup() {
+  createCanvas(SCREEN_SIZE, SCREEN_SIZE);
+  
+}
+// Draw
+function draw(){
+  background(51);
+}
+
+// Action
--- public/CC/14_FractalRec/sketch.js
+++ public/CC/14_FractalRec/sketch.js
@@ -1,7 +1,38 @@
 // globals
+var angle = 0;
+var slider;
 
 // constant
+const SCREEN_SIZE = 600;
 
 // Setup
-
+function setup() {
+  createCanvas(SCREEN_SIZE, SCREEN_SIZE);
+  slider = createSlider(0, TWO_PI, PI / 4, 0.01);
+}
 // Draw
+function draw(){
+  background(51);
+  angle = slider.value();
+  stroke(255);
+  translate(SCREEN_SIZE/2, SCREEN_SIZE);
+  branch(150);
+}
+
+// Action
+function branch(len) {
+  line(0, 0, 0, -len);
+  translate(0, -len);
+  if(len > 4){
+    // recursive call to right
+    push();
+    rotate(angle);
+    branch(len * 0.7);
+    pop();
+    // recursive call to left
+    push();
+    rotate(-angle);
+    branch(len * 0.71);
+    pop();
+  }
+}

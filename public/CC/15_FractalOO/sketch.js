a783d4dd984cff3e1008b51b2b24d4fef16c39fb
MAshraf <ee.mashraf@gmail.com>
2018-09-17 13:10:44 +0300

    cc15 sketch structure

---
+++ public/CC/15_FractalOO/sketch.js
@@ -0,0 +1,57 @@
+// globals
+var tree = [];
+var leaves = [];
+
+var count = 0;
+
+// constant
+const SCREEN_SIZE = 400;
+const TREE_LVL = 6;
+
+// Setup
+function setup() {
+  createCanvas(SCREEN_SIZE, SCREEN_SIZE);
+  let a = createVector(SCREEN_SIZE / 2, SCREEN_SIZE);
+  let b = createVector(SCREEN_SIZE / 2, SCREEN_SIZE - 100);
+  let root = new Branch(a, b);
+  
+  tree[0] = root;
+}
+
+// Control
+function mousePressed() {
+  for(let i = tree.length - 1; i >= 0; i--){
+    if(!tree[i].finished){
+      tree.push(tree[i].branchA());
+      tree.push(tree[i].branchB());
+    }
+    tree[i].finished = true;
+  }
+  count++;
+  
+  if(count === TREE_LVL){
+    for(let i = 0; i < tree.length; i++){
+      if(!tree[i].finished){
+        let leaf = tree[i].end.copy();
+        leaves.push(leaf);
+      }
+    }
+  }
+}
+// Draw
+function draw(){
+  background(51);
+  
+  for(let i = 0; i < tree.length; i++){
+    tree[i].show();
+  }
+  
+  for(let i = 0; i < leaves.length; i++){
+    fill(255, 0, 100, 100);
+    noStroke();
+    ellipse(leaves[i].x, leaves[i].y, 8, 8);
+    leaves[i].y += random(0,2);
+  }
+}
+
+// Action

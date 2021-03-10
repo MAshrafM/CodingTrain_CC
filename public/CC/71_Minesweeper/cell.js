1295a7b688d8fd473b85037f6c94a66ad6c98ded
MAshrafM <ee.mashraf@gmail.com>
2021-03-10 14:07:54 +0200

    add mines and numbers

--- public/CC/71_Minesweeper/cell.js
+++ public/CC/71_Minesweeper/cell.js
@@ -1,12 +1,13 @@
 class Cell {
     constructor(i, j, w) {
-        this.x = i;
-        this.y = j;
+        this.i = i;
+        this.j = j;
         this.x = i * w;
         this.y = j * w;
         this.w = w;
-        this.bee = true;
+        this.mine = false;
         this.revealed = false;
+        this.neighborCount = 0;
     }
 
     show(){
@@ -14,7 +15,7 @@ class Cell {
         noFill();
         rect(this.x, this.y, this.w, this.w);
         if(this.revealed){
-            if(this.bee){
+            if(this.mine){
                 stroke(0)
                 fill(128);
                 ellipse(this.x + this.w * 0.5, this.y + this.w * 0.5, this.w * 0.5);
@@ -23,6 +24,11 @@ class Cell {
                 fill(200);
                 noStroke();
                 rect(this.x, this.y, this.w, this.w);
+                if(this.neighborCount > 0){
+                    textAlign(CENTER);
+                    fill(0)
+                    text(this.neighborCount, this.x + this.w * 0.5, this.y + this.w - 15);
+                }
             }
         }
     }
@@ -33,6 +39,27 @@ class Cell {
 
     reveal(){
         this.revealed = true;
+    }
+
+    countNeighbors(){
+        if(this.mine){
+            return -1;
+        }
+        let total = 0;
+        for(let i = -1; i <=1; i++){
+            let idx = this.i + i;
+            if(idx < 0 || idx >= cols) continue;
+            for(let j = -1; j <=1; j++){     
+                let jdx = this.j + j;
+                if(jdx <0 || jdx >= rows) continue;
+                console.log(idx + " : " + jdx)
+                let neighbor = grid[idx][jdx];
+                if(neighbor.mine){
+                    total++;
+                }
+            }
+        }
+        this.neighborCount = total;
     }
 }
 
--- public/CC/71_Minesweeper/sketch.js
+++ public/CC/71_Minesweeper/sketch.js
@@ -11,6 +11,7 @@ const SCREEN = 400;
 const w = 40;
 const cols = SCREEN / w;
 const rows = SCREEN / w;
+const mines = 20;
 let grid;
 
 function setup(){
@@ -19,6 +20,29 @@ function setup(){
     for(let i = 0; i < cols; i++){
         for(let j = 0; j < rows; j++){
             grid[i][j] = new Cell(i, j, w);
+        }
+    }
+
+    // add mines
+    let options = [];
+    for(let i = 0; i < cols; i++){
+        for(let j = 0; j < rows; j++){
+            options.push([i, j]);
+        }
+    }
+
+    for(let m = 0; m < mines; m++){
+        let idx = Math.floor(random(options.length));
+        let choice = options[idx];
+        let x = choice[0],
+            y = choice[1];
+        options.splice(idx, 1);
+        grid[x][y].mine = true;
+    }
+
+    for(let i = 0; i < cols; i++){
+        for(let j = 0; j < rows; j++){
+            grid[i][j].countNeighbors();
         }
     }
 
